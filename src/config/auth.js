3c9e92a6a87a80f9f8b230517c642489c3b734cb
Tolfx <matteus12122@gmail.com>
2020-08-27 17:03:11 +0200

    Changed routes

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,21 +1,21 @@
 module.exports = {
-    ensureAuthenticated: function (req, res, next) {
-      if (req.isAuthenticated()) {
-        return next();
-      }
-      req.flash('error_msg', 'Please log in to view that resource');
-      res.redirect('/users/login');
-    },
-    forwardAuthenticated: function (req, res, next) {
-      if (!req.isAuthenticated()) {
-        return next();
-      }
-      res.redirect('/dashboard');
-    },
-    ensureIsAdmin: function (req, res, next) {
-      if (req.user.role !== 'admin') {
-        res.redirect('/dashboard');
-      }
-      next();
-    },
-  };
+  ensureAuthenticated: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash('error_msg', 'Please log in to view that resource');
+    res.redirect('/login');
+  },
+  forwardAuthenticated: function (req, res, next) {
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/lobby');
+  },
+  ensureIsAdmin: function (req, res, next) {
+    if (req.user.role !== 'admin') {
+      res.redirect('/lobby');
+    }
+    next();
+  },
+};
