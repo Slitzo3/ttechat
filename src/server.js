a660b95a9ff39ba19e50bc91435078afe0298cd8
Tolfx <matteus12122@gmail.com>
2020-08-27 19:48:35 +0200

    Added cannotfindpage

--- src/server.js
+++ src/server.js
@@ -69,5 +69,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 app.get('*', (req, res) => {
-  res.status(404).send('Cannot find page');
+  res.status(404).render('cannotFindPage');
 });
