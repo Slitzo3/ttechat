dcc63eef26664f59d37e83db7b45d626fc5234b0
Tolfx <matteus12122@gmail.com>
2020-08-27 20:03:12 +0200

    Added comments on auth

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,5 @@
 module.exports = {
+  //Makes sure the user is logged in otherwise will get back to the login route.
   ensureAuthenticated: function (req, res, next) {
     if (req.isAuthenticated()) {
       return next();
@@ -6,12 +7,14 @@ module.exports = {
     req.flash('error_msg', 'Please log in to view that resource');
     res.redirect('/login');
   },
+  //Makes sure those who are logged in can't access the route, only those who aren't singed in session.
   forwardAuthenticated: function (req, res, next) {
     if (!req.isAuthenticated()) {
       return next();
     }
     res.redirect('/lobby');
   },
+  //Makes sure the user is an admin otherwises gets kicked back to lobby.
   ensureIsAdmin: function (req, res, next) {
     if (req.user.role !== 'admin') {
       res.redirect('/lobby');
