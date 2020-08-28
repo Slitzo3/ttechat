4b857502f13a44fd50543ced7c72a0cef557b865
Tolfx <matteus12122@gmail.com>
2020-08-28 17:03:46 +0200

    Added new get routes for lobby

--- src/routes/lobby.js
+++ src/routes/lobby.js
@@ -9,4 +9,16 @@ router.get('/', ensureAuthenticated, (req, res) => {
   res.render('./lobby/lobby', { users: req.user });
 });
 
+router.get('/friends', ensureAuthenticated, (req, res) => {
+  res.render('./lobby/friends', { users: req.user });
+});
+
+router.get('/servers', ensureAuthenticated, (req, res) => {
+  res.render('./lobby/servers', { users: req.user });
+});
+
+router.get('/create', ensureAuthenticated, (req, res) => {
+  res.render('./lobby/create', { users: req.user });
+});
+
 module.exports = router;
