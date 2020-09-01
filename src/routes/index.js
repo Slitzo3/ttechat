ef6ee28d0aaf38e9d1fed26ff2d47f73fe0914c2
Tolfx <matteus12122@gmail.com>
2020-09-01 21:17:06 +0200

    ensure

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const User = require('../models/User');
-const { forwardAuthenticated } = require('../config/auth');
+const { forwardAuthenticated, ensureAuthenticated } = require('../config/auth');
 
 router.get('/', forwardAuthenticated, (req, res) => {
   res.render('getstarted');
