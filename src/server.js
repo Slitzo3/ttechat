2e490f903fd3e85cbc3b809c5560bd60e3ad79bf
Tolfx <matteus12122@gmail.com>
2020-09-01 21:16:42 +0200

    Public and socket

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-const express = require('express');
-const app = require('express')();
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
-const methodOverride = require('method-override');
+const express = require("express");
+const app = require("express")();
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 mongoose.connect(process.env.MONGODB_NAV, {
   useNewUrlParser: true,
@@ -21,12 +21,16 @@ mongoose.connect(process.env.MONGODB_NAV, {
 
 const db = mongoose.connection;
 
+//Sockets
+require("./socket/messages")(db);
+
 // EJS
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 //Override
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
@@ -37,7 +41,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
-  })
+  }),
 );
 
 // Passport middleware
@@ -49,25 +53,25 @@ app.use(flash());
 
 // Global variables
 app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
 //Routes
-app.use('/', require('./routes/index.js'));
-app.use('/lobby', require('./routes/lobby.js'));
+app.use("/", require("./routes/index.js"));
+app.use("/lobby", require("./routes/lobby.js"));
 
 //On error [MongoDB]
-db.on('error', (error) => console.log(error));
+db.on("error", (error) => console.log(error));
 //On open [MongoDB]
-db.once('open', () => console.log('Connected to database'));
+db.once("open", () => console.log("Connected to database"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
-app.get('*', (req, res) => {
-  res.status(404).render('cannotFindPage');
+app.get("*", (req, res) => {
+  res.status(404).render("cannotFindPage");
 });
