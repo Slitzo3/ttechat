7176b01c4a12c71bac1d7dfa1e216945a545bf8a
Tolfx <matteus12122@gmail.com>
2020-09-01 21:17:19 +0200

    Formatting

--- src/routes/lobby.js
+++ src/routes/lobby.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-const User = require('../models/User');
-const { forwardAuthenticated, ensureAuthenticated } = require('../config/auth');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const User = require("../models/User");
+const { forwardAuthenticated, ensureAuthenticated } = require("../config/auth");
 
-router.get('/', ensureAuthenticated, (req, res) => {
-  res.render('./lobby/lobby', { users: req.user });
+router.get("/", ensureAuthenticated, (req, res) => {
+  res.render("./lobby/lobby", { users: req.user });
 });
 
-router.get('/friends', ensureAuthenticated, (req, res) => {
-  res.render('./lobby/friends', { users: req.user });
+router.get("/friends", ensureAuthenticated, (req, res) => {
+  res.render("./lobby/friends", { users: req.user });
 });
 
-router.get('/servers', ensureAuthenticated, (req, res) => {
-  res.render('./lobby/servers', { users: req.user });
+router.get("/servers", ensureAuthenticated, (req, res) => {
+  res.render("./lobby/servers", { users: req.user });
 });
 
-router.get('/create', ensureAuthenticated, (req, res) => {
-  res.render('./lobby/create', { users: req.user });
+router.get("/create", ensureAuthenticated, (req, res) => {
+  res.render("./lobby/create", { users: req.user });
 });
 
 module.exports = router;
