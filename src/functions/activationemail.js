d56eb5631726ce973bca4b06202eeec1eab02c8c
Albin Håkanson <33166817+Tooxic@users.noreply.github.com>
2020-09-03 17:25:30 +0200

    Added things :)

---
+++ src/functions/activationemail.js
@@ -0,0 +1,43 @@
+"use strict";
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "sweplox.se",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL, // generated ethereal user
+      pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+    },
+  });
+
+   let account = "Tooxic"
+   let email = "albin@sweplox.se"
+   var crypto = require("crypto");
+   var conf = crypto.randomBytes(10).toString('hex');
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"TTE Chat" <ttechat@sweplox.se>', // sender address
+    to: `${email}`, // list of receivers
+    subject: `Activation email for ${account}`, // Subject line
+    text: `Activation email for ${account}`, // plain text body
+    html: `Activation email for ${account}, https://ttechat.sweplox.se/activation/${conf}`, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
