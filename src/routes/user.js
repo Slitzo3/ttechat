491f3a1449d2ca1f46997ad6f73931e521120e2a
Tolfx <matteus12122@gmail.com>
2020-09-14 18:38:39 +0200

    ✨Modified < user.js & edit.ejs >

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,55 @@
 const express = require('express');
 const router = express.Router();
-const { forwardAuthenticated, ensureAuthenticated } = require("../config/auth");
+const User = require('../models/User');
+const { forwardAuthenticated, ensureAuthenticated } = require('../config/auth');
 
 router.get('/edit', ensureAuthenticated, (req, res) => {
-    res.render('./user/edit')
+  res.render('./user/edit', {
+    user: req.user,
+  });
+});
+
+router.post('/edit', ensureAuthenticated, (req, res) => {
+  const { username, profile, Newpassword, Newpassword2 } = req.body;
+  let errors = [];
+  let newPassword = [];
+  newPassword.password = 'none';
+
+  const newName = username ? username : req.user.username;
+  const newProfile = profile != req.user.profilePicture ? profile : req.user.profilePicture;
+
+  if (Newpassword) {
+    if (Newpassword === Newpassword2) {
+      newPassword.push({
+        password: Newpassword,
+      });
+    } else {
+      errors.push({
+        msg: "Password doesn't match",
+      });
+      res.render('./user/edit', {
+        errors,
+        username,
+        email,
+        password,
+        password2,
+      });
+    }
+  }
+  User.findOne({ email: req.user.email }).then((user) => {
+    user.username = newName;
+    user.profilePicture = newProfile;
+    user.password = newPassword.password === 'none' ? user.password : newPassword.password;
+    errors.push({
+      success_msg: 'Succesfully changed',
+    });
+    user.save();
+  });
+
+  res.render('./user/edit', {
+    errors,
+    user: req.user,
+  });
 });
 
 module.exports = router;
